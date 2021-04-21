b2728ef98fceb890628ebbc31160181795da071a
keyanoush <keyanoush@Keyanoush.localdomain>
2021-04-21 14:37:29 -0500

    first commit

---
+++ leonids-toys.js
@@ -0,0 +1,30 @@
+const Basketball = {
+    id: 1,
+    name: "Basketball",
+    maker: "Spalding",
+    material: "Rubber",
+    price: 15,
+    weight: 22,
+}
+
+const SoccerBall = {
+    id: 2,
+    name: "Soccer Ball",
+    maker: "Nike",
+    material: "Plastic",
+    price: 30,
+    weight: 16,
+}
+
+const Football = {
+    id: 3,
+    name: "Football",
+    maker: "Riddell",
+    material: "Leather",
+    price: 20,
+    weight: 16, 
+}
+
+const toys =[Basketball,SoccerBall,Football]
+
+console.log(toys)
